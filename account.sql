6f58cb692743e64e31a60d18773511dbb9fe9e14
香菜超级难吃 <11419555+yhy20000921@user.noreply.gitee.com>
2023-03-13 06:56:30 +0000

    sql文件 1
    Signed-off-by: 香菜超级难吃 <11419555+yhy20000921@user.noreply.gitee.com>

---
+++ account.sql
@@ -0,0 +1,8 @@
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('1111111112', '导读的', 1, 33.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('1211111111', '分分分', 1, 0.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('1222222222', '烦烦烦', 1, 0.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('1901190207', '杨胡燚', 0, 9999.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('2111111111', '导读的', 1, 0.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('2222222220', '丁顶', 1, 0.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('2222222221', '丁顶', 1, 0.00);
+INSERT INTO `account` (`id`, `name`, `status`, `balance`) VALUES ('2222222223', '踩踩踩从', 1, 500.00);
---
+++ details.sql
@@ -0,0 +1,13 @@
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1632773368983908353, 11.00, '2023-03-07 00:01:21', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1632773818760097793, 11.00, '2023-03-07 00:03:09', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1632776085127147521, -1.50, '2023-03-07 00:12:09', 1628604306791866370, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1632777902963564546, 1.50, '2023-03-07 00:19:22', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1632778659318214658, 1.00, '2023-03-07 00:22:23', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1632931636490317826, 500.00, '2023-03-07 10:30:15', 1, 2222222223);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633406363432386561, -1.00, '2023-03-08 17:56:39', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633406710301327361, -50.00, '2023-03-08 17:58:02', 1, 1222222222);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633408014729871361, -1.00, '2023-03-08 18:03:13', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633409078288031745, 100.00, '2023-03-08 18:07:26', 1, 1222222222);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633409154586615810, -50.00, '2023-03-08 18:07:44', 1, 1222222222);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633409712512847874, -1.00, '2023-03-08 18:09:57', 1, 1111111112);
+INSERT INTO `details` (`id`, `balance`, `update_time`, `update_user`, `account_id`) VALUES (1633410308259270658, -1.00, '2023-03-08 18:12:19', 1, 1111111112);
